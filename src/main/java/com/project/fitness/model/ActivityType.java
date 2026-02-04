77f7034e786ca9cf6277bdbb606e5a1b8e153568
Swastik Banerjee <swastikb.aot@gmail.com>
2026-02-04 22:53:20 +0530

    Database config, lombok

---
+++ src/main/java/com/project/fitness/model/ActivityType.java
@@ -0,0 +1,14 @@
+package com.project.fitness.model;
+
+public enum ActivityType {
+    WALKING,
+    CYCLING,
+    RUNNING,
+    SWIMMING,
+    WEIGHT_TRAINING,
+    YOGA,
+    HIIT,
+    CARDIO,
+    STRECHING,
+    OTHER
+}
